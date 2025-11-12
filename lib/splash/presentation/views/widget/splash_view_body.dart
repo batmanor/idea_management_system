38a8476b4e33f669ab3cc0cf1ec85e7f77b13874
akrame ali <akrame@gmail.com>
2025-11-12 17:12:38 +0300

    design splah view by akram

---
+++ lib/splash/presentation/views/widget/splash_view_body.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:splash_view/feature/login/presentation/home_page.dart';
+
+class SplashViewBody extends StatefulWidget {
+  @override
+  _SplashViewBodyState createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    init_animation();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFFE0F2F7), // أزرق سماوي فاتح جداً
+              Color.fromARGB(
+                255,
+                82,
+                93,
+                153,
+              ), // بنفسجي باهت جداً (Lavender/Periwinkle)
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: ScaleTransition(
+            scale: _scaleAnimation,
+            child: FadeTransition(
+              opacity: _fadeAnimation,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assests/0a372a30-5f5c-4779-9428-75f97330205c.png',
+                    width: 250,
+                    height: 250,
+                  ),
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void init_animation() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.5, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.elasticOut),
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
+    );
+
+    _animationController.forward();
+
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Future.delayed(Duration(milliseconds: 500), () {
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (context) => HomePage()));
+        });
+      }
+    });
+  }
+}
